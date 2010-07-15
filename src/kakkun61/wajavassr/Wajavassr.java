f32f228370fe0ee0f3c749594ffcd478f3a58e77
Kazuki Okamoto <kakkun61@gmail.com>
2010-07-16 01:40:39 +0900

    ソースファイルの追加

---
+++ src/kakkun61/wajavassr/Wajavassr.java
@@ -0,0 +1,155 @@
+package kakkun61.wajavassr;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.codec.binary.Base64;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class Wajavassr
+{
+	protected final String wassr = "http://api.wassr.jp";
+	protected String auth = null;
+	protected static final String DEFAULT_USER_AGENT = "Wajavassr/00.03";
+	protected String userAgent = DEFAULT_USER_AGENT;
+	protected final JSONParser parser = new JSONParser();
+
+	/**
+	 * 後で必ず {@link #setUser(String, String)} を呼び出すこと。
+	 */
+	public Wajavassr(){}
+
+	public Wajavassr( String user, String password )
+	{
+		setUser( user, password );
+	}
+
+	public void setUser( String user, String password )
+	{
+		auth = createAuthentication( user, password );
+	}
+
+	/**
+	 * authentication の作成
+	 * @param user ログイン名
+	 * @param password パスワード or 認証トークン
+	 * @return authentication
+	 */
+	protected static String createAuthentication( String user, String password )
+	{
+		return new String( Base64.encodeBase64( ( user + ":" + password ).getBytes() ) );
+	}
+
+	public String getUserAgent()
+	{
+		return userAgent;
+	}
+
+	/**
+	 * User Agent を設定する。引数 {@code userAgent} が {@code null} の場合デフォルト値に設定。
+	 * @param userAgent 新たに設定する User Agent。{@code null} の場合デフォルト値に設定。
+	 */
+	public void setUserAgent( String userAgent )
+	{
+		if( userAgent == null )
+			this.userAgent = DEFAULT_USER_AGENT;
+		else
+			this.userAgent = userAgent;
+	}
+
+	/**
+	 * Wassr に対し、指定されたパスに指定された HTTP メソッドでコネクションを張る。
+	 * @param method HTTP メソッド。GET、POST など。
+	 * @param path ルート以下のパス。“http://api.wassr.jp/statuses/friends_timeline.json” の場合、“/statuses/friends_timeline.json”
+	 * @return 確立したコネクション
+	 * @throws IOException コネクションの確立に失敗。
+	 */
+	protected URLConnection createURLConnection( String method, String path ) throws IOException
+	{
+		HttpURLConnection c = (HttpURLConnection)new URL( wassr + path ).openConnection();
+		c.setRequestMethod( method );
+		c.setRequestProperty( "User-Agent", userAgent );
+		c.setRequestProperty( "Authorization", "Basic " + auth );
+		return c;
+	}
+
+	public JSONArray getReplies() throws IOException,
+	                                     ParseException
+	{
+		URLConnection c = createURLConnection( "GET", "/statuses/replies.json" );
+		c.connect();
+		BufferedReader r = new BufferedReader( new InputStreamReader( c.getInputStream(), "UTF-8" ) );
+		JSONArray json;
+		try
+		{
+			json = (JSONArray)parser.parse( r );
+		}
+		finally
+		{
+			r.close();
+		}
+		return json;
+	}
+
+	/**
+	 *
+	 * @return
+	 * @throws IOException コネクションの確立に失敗、または読み込みの失敗。
+	 * @throws ParseException JSON のパースに失敗。
+	 */
+	public List<Hitokoto> getFriendTimelie() throws IOException, ParseException
+	{
+		List<Hitokoto> hitokotos = new ArrayList<Hitokoto>();
+		URLConnection c = createURLConnection( "GET", "/statuses/friends_timeline.json" );
+		BufferedReader r = new BufferedReader( new InputStreamReader( c.getInputStream(), "UTF-8" ) );
+		JSONArray jhs; // Json Hitokotos
+		try
+		{
+			jhs = (JSONArray)parser.parse( r );
+		}
+		finally
+		{
+			r.close();
+		}
+		for( Object o : jhs )
+		{
+			JSONObject jh = (JSONObject)o;
+			JSONObject user = (JSONObject)jh.get( "user" );
+			Object[] favorites = ( (JSONArray)jh.get( "favorites" ) ).toArray();
+			hitokotos.add(
+					new Hitokoto(
+							(String)jh.get( "html" ),
+							(String)jh.get( "text" ),
+							(String)jh.get( "id" ),
+							(String)jh.get( "link" ),
+							(String)jh.get( "user_login_id" ),
+							(String)user.get( "screen_name" ),
+							(String)user.get( "profile_image_url" ),
+							(Boolean)user.get( "protected" ),
+							(String)jh.get( "photo_url" ),
+							(String)jh.get( "photo_thombnail_url" ),
+							Arrays.copyOf( favorites, favorites.length, String[].class ),
+							(String)jh.get( "reply_message" ),
+							(String)jh.get( "reply_status_url" ),
+							(String)jh.get( "reply_user_login_id" ),
+							(String)jh.get( "reply_user_nick" ),
+							(String)jh.get( "areaname" ),
+							(String)jh.get( "areacode" ),
+							(Long)jh.get( "epoch" ),
+							(String)jh.get( "slurl" )
+					)
+			);
+		}
+		return hitokotos;
+	}
+}
